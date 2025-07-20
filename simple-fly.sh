07175b518c877fbbc4ed1e3d8827feff80839e76
Tony Stoyanov <tnstoyanov@gmail.com>
2025-07-20 16:28:11 +0300

    Remove obsolete deployment scripts and guides for various platforms

--- simple-fly.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Installing Fly.io CLI..."
-curl -L https://fly.io/install.sh | sh
-export PATH="$PATH:$HOME/.fly/bin"
-cd /Users/tonystoyanov/Documents/cashier
-flyctl auth login
-flyctl launch --no-deploy --name webcashier
-flyctl deploy
--- start-app.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /Users/tonystoyanov/Documents/cashier/WebCashier
-echo "Starting WebCashier application..."
-dotnet run --launch-profile https
