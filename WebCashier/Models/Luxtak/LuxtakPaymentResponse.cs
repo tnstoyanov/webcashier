dae6ddde76b2bd41df6060cd52cbb8cdf79297df
Tony Stoyanov <tnstoyanov@gmail.com>
2025-08-21 16:25:59 +0300

    Restore files from commit 2844eca

--- WebCashier/Models/Luxtak/LuxtakPaymentResponse.cs
+++ WebCashier/Models/Luxtak/LuxtakPaymentResponse.cs
@@ -10,8 +10,6 @@ namespace WebCashier.Models.Luxtak
         [JsonPropertyName("msg")]
         public string Message { get; set; } = string.Empty;
 
-<<<<<<< HEAD
-=======
         [JsonPropertyName("sub_code")]
         public string? SubCode { get; set; }
 
@@ -39,17 +37,22 @@ namespace WebCashier.Models.Luxtak
         [JsonPropertyName("trade_no")]
         public string TradeNo { get; set; } = string.Empty;
 
->>>>>>> 2844ecadaec88cc3e03b7e14ca37b489d4d37aff
         [JsonPropertyName("out_trade_no")]
         public string OutTradeNo { get; set; } = string.Empty;
 
-        [JsonPropertyName("trade_no")]
-        public string TradeNo { get; set; } = string.Empty;
+        [JsonPropertyName("order_amount")]
+        public string OrderAmount { get; set; } = string.Empty;
 
-        [JsonPropertyName("web_url")]
-        public string WebUrl { get; set; } = string.Empty;
+        [JsonPropertyName("order_currency")]
+        public string OrderCurrency { get; set; } = string.Empty;
 
-        [JsonPropertyName("prepay_id")]
-        public string PrepayId { get; set; } = string.Empty;
+        [JsonPropertyName("trade_status")]
+        public string TradeStatus { get; set; } = string.Empty;
+
+        [JsonPropertyName("payment_url")]
+        public string PaymentUrl { get; set; } = string.Empty;
+
+        [JsonPropertyName("timestamp")]
+        public string Timestamp { get; set; } = string.Empty;
     }
 }
